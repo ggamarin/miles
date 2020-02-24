2bad9b72f644ef719e3849e4373862da4c5a5993
Grigoriy Gamarin <gam110191@gmail.com>
2020-02-24 19:20:38 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 152347.47;
+        int spend = 20;
+        double miles = price / spend;
+        System.out.println(miles);
+    }
+}
